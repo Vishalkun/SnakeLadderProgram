0322ae5f03dda758a1fae3407dbcb7cca0f48531
Vishal github <Vishal.kunchal@gmail.com>
2023-09-27 17:13:43 +0530

    [vishal kunchal]: Player At Zero Position

--- SnakeLadder.cs
+++ SnakeLadder.cs
@@ -14,16 +14,6 @@ namespace SnakeAndLadderPrograms
             int player_one;
             player_one = position;
             Console.WriteLine("player one position is {0}", player_one);
-            player_one = roll_die();
-            Console.WriteLine("player one rolled the dice and got the position {0}", player_one);
-        }
-
-        public int roll_die()
-        {
-            Random random = new Random();
-            int dice = random.Next(1, 7);
-            return dice;
-
         }
         public void Board()
         {
