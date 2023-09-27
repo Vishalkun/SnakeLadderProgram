b316a434b6b94c06c5acfb8164b3df7e38da7103
Vishal github <Vishal.kunchal@gmail.com>
2023-09-27 16:55:54 +0530

    [vishal kunchal]: Player At Zero

--- SnakeLadder.cs
+++ SnakeLadder.cs
@@ -12,44 +12,88 @@ namespace SnakeAndLadderPrograms
         {
             int position;
             int player_one = 3;
+            int check_win;
 
             Console.WriteLine("player one position is {0}", player_one);
-            position = roll_die();
-            if (position == 0)
-            {
-                Console.WriteLine("no play");
-                player_one += position;
 
-            }
-            if (player_one == 0 && position < 0)
+            while (player_one <= 100)
             {
-                Console.WriteLine("this is snake bite @ 0");
-                player_one = 0;
-            }
-
-            if (player_one > 0 && position < 0)
-            {
-                Console.WriteLine("this is a sanke bite");
-                player_one += position;
-                if (player_one < 0)
+                check_win = CheckWin(player_one);
+                if (check_win == 1)
                 {
+                    Console.WriteLine("player won the game!!!");
+                    break;
+
+                }
+                if (check_win == 2)
+                {
+                    position = 0;
+
+                }
+                else
+                {
+                    position = roll_die();
+                }
+
+                if (position == 0)
+                {
+                    Console.WriteLine("no play");
+                    player_one += position;
+
+                }
+                if (player_one == 0 && position < 0)
+                {
+                    Console.WriteLine("this is snake bite @ 0");
                     player_one = 0;
                 }
+                if (player_one > 0 && position < 0)
+                {
+                    Console.WriteLine("this is a sanke bite");
+                    player_one += position;
+                    if (player_one < 0)
+                    {
+                        player_one = 0;
+                    }
+                }
+                if (position > 0)
+                {
+                    Console.WriteLine("its a ladder");
+                    player_one += position;
+                }
+                if (player_one > 100)
+                {
+
+                    player_one -= position;
+                }
+                Console.WriteLine("player one rolls the die and get the position {0}", player_one);
             }
-            if (position > 0)
+        }
+
+        public int CheckWin(int player_one)
+        {
+            if (player_one == 100)
             {
-                Console.WriteLine("its a ladder");
-                player_one += position;
+                return 1;
             }
-            Console.WriteLine("player one rolls the die and get the position {0}", player_one);
+            if (player_one > 100)
+            {
+                return 2;
+            }
+            else
+            {
+                return 0;
+            }
+
         }
 
         readonly Random random = new Random();
+        int dice_thrown = 0;
 
         public int roll_die()
         {
             int dice, check;
             dice = random.Next(1, 7);
+            dice_thrown++;
             Console.WriteLine("dice={0} ", dice);
             check = check_play();
 
@@ -76,6 +120,7 @@ namespace SnakeAndLadderPrograms
         public void Board()
         {
             start();
+            Console.WriteLine("number of time dice thrown is {0}", dice_thrown);
 
         }
     }
